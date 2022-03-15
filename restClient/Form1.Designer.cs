2b181a9283f651595cca14a428d6711d4fcdeb81
KAMİL LÜLECİ <kamilluleci@gmail.com>
2022-03-15 22:01:06 +0300

    Add project files.

---
+++ restClient/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace restClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRestURI = new System.Windows.Forms.TextBox();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRestURI
+            // 
+            this.txtRestURI.Location = new System.Drawing.Point(121, 27);
+            this.txtRestURI.Name = "txtRestURI";
+            this.txtRestURI.Size = new System.Drawing.Size(432, 20);
+            this.txtRestURI.TabIndex = 0;
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(121, 68);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(646, 206);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Location = new System.Drawing.Point(559, 24);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(208, 23);
+            this.cmdGO.TabIndex = 2;
+            this.cmdGO.Text = "GO!";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.cmdGO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Request URI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Response:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 371);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtRestURI);
+            this.Name = "Form1";
+            this.Text = "C# REST Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRestURI;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button cmdGO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ restClient/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region UI Event Handlers
+        private void cmdGO_Click(object sender, EventArgs e)
+        {
+            //Test urls (URI)
+            //https://dry-cliffs-19849.herokuapp.com/
+            //https://dry-cliffs-19849.herokuapp.com/users
+            //https://dry-cliffs-19849.herokuapp.com/users.json
+            //https://dry-cliffs-19849.herokuapp.com/users/2.html
+
+            RestClient restClient = new RestClient();
+            restClient.endPoint = txtRestURI.Text;
+
+            debugOutput("Rest Client Created");
+            
+            string strResponse = string.Empty;
+
+            strResponse = restClient.makeRequest();
+
+            debugOutput(strResponse);
+
+
+        }
+
+        #endregion
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText+Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message,ToString()+Environment.NewLine);
+            }
+        }
+    }
+}
